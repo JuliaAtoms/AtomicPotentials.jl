d455e651e6ec967f5400f37f3bea01b889afb826
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-01-28 21:27:49 +0100

    Ground state configurations of all elements

--- src/AtomicPotentials.jl
+++ src/AtomicPotentials.jl
@@ -1,25 +1,14 @@
 module AtomicPotentials
+using AtomicLevels
 
 abstract type AbstractPotential{T} end
 
 # * Table of elements
 
-const table_of_elements = [:H, :He, :Li, :Be, :B, :C, :N, :O, :F, :Ne,
-                           :Na, :Mg, :Al, :Si, :P, :S, :Cl, :Ar, :K,
-                           :Ca, :Sc, :Ti, :V, :Cr, :Mn, :Fe, :Co, :Ni,
-                           :Cu, :Zn, :Ga, :Ge, :As, :Se, :Br, :Kr,
-                           :Rb, :Sr, :Y, :Zr, :Nb, :Mo, :Tc, :Ru, :Rh,
-                           :Pd, :Ag, :Cd, :In, :Sn, :Sb, :Te, :I, :Xe,
-                           :Cs, :Ba, :La, :Ce, :Pr, :Nd, :Pm, :Sm,
-                           :Eu, :Gd, :Tb, :Dy, :Ho, :Er, :Tm, :Yb,
-                           :Lu, :Hf, :Ta, :W, :Re, :Os, :Ir, :Pt, :Au,
-                           :Hg, :Tl, :Pb, :Bi, :Po, :At, :Rn, :Fr,
-                           :Ra, :Ac, :Th, :Pa, :U, :Np, :Pu, :Am, :Cm,
-                           :Bk, :Cf, :Es, :Fm, :Md, :No, :Lr, :Rf,
-                           :Db, :Sg, :Bh, :Hs, :Mt]
+include("table_of_elements.jl")
 
 function element_number(s::Symbol)
-    i = findfirst(isequal(s), table_of_elements)
+    i = findfirst(e -> e[1] == s, table_of_elements)
     i === nothing &&
         throw(ArgumentError("Invalid element $s"))
     i
@@ -37,8 +26,10 @@ macro pc_str(s)
     :(PointCharge(Symbol($s)))
 end
 
-Base.show(io::IO, p::PointCharge{I}) where {I<:Integer} =
-    write(io, "Z = $(p.Z) [$(table_of_elements[p.Z])]")
+function Base.show(io::IO, p::PointCharge{I}) where {I<:Integer}
+    element = table_of_elements[p.Z]
+    write(io, "Z = $(p.Z) [$(element[2][1])]")
+end
 
 Base.show(io::IO, p::PointCharge) =
     write(io, "Z = $(p.Z)")
@@ -47,6 +38,12 @@ Base.show(io::IO, p::PointCharge) =
 
 charge(p::PointCharge) = p.Z
 
-export AbstractPotential, element_number, PointCharge, @pc_str, charge
+ground_state(p::PointCharge{<:Integer}) =
+    table_of_elements[p.Z][2][2]
+
+ground_state(p::PointCharge) =
+    throw(ArgumentError("Ground state configuration for nuclear charge of Z = $(p.Z) unknown"))
+
+export AbstractPotential, element_number, PointCharge, @pc_str, charge, ground_state
 
 end # module
